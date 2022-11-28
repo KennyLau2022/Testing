1d24256214ca2e6831d1ceb0bfa6ddd097dceea1
root <root@DESKTOP-TIV4AVP.localdomain>
2022-11-28 22:36:12 +0800

    KennyLau

---
+++ app.js
@@ -0,0 +1,31 @@
+let digit = 0;
+let counter = document.querySelector(".counter");
+const increase = document.querySelector(".increase");
+const decrease = document.querySelector(".decrease");
+const reset = document.querySelector(".reset");
+decrease.addEventListener("click", function () {
+    if (digit == 0){
+        alert("We cannot go lower than 0!");
+    } else {
+        digit--;
+        console.log(digit);
+        counter.innerHTML = digit;
+    }
+});
+increase.addEventListener("click", function () {
+    if (digit >= 5){
+        alert("We cannot go greater than 50!");
+    } else {
+        
+        digit++;
+        console.log(digit);
+        counter.innerHTML = digit;
+    }
+    
+    
+});
+reset.addEventListener("click", function () {
+    digit = 0
+    console.log(digit);
+    counter.innerHTML = digit;
+});
